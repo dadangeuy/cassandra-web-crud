8d008f0a597038f1113173fc9813b11408082c62
dadangeuy <irsyad.rizaldi97@gmail.com>
2018-11-23 01:54:36 +0700

    Initial Project

---
+++ src/main/java/com/rizaldi/cassandra/web/crud/CassandraWebCrudApplication.java
@@ -0,0 +1,12 @@
+package com.rizaldi.cassandra.web.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraWebCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraWebCrudApplication.class, args);
+    }
+}
